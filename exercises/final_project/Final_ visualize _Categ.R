6c17aaf62e19ed794f3db9b34bc827910d73ce57
Aubrey9922 <47202411+Aubrey9922@users.noreply.github.com>
2020-05-10 14:19:24 -0500

    Update Final_ visualize _Categ.R

--- exercises/final_project/Final_ visualize _Categ.R
+++ exercises/final_project/Final_ visualize _Categ.R
@@ -19,88 +19,85 @@ library(fastDummies)
 
 
 
-
 bank = read_delim("~/Desktop/SDS 323/final/data/bank-additional-full.csv", delim = ";")
 bank10 = read_delim("~/Desktop/SDS 323/final/data/bank-additional.csv", delim = ";")
 
 # get rid of observations that have unknowns
 # bank10 %>% mutate_if(is.character, list(~na_if(., "unknown"))) %>% na.omit()
-bank10 <- bank10 %>% replace_with_na_all(condition = ~.x == "unknown")
-bank10 <-bank10[complete.cases(bank10), ] # went from 4119 to 3090 obs
-
-
-# add dummy variables for housing, loan, target variable
-#bank10 = mutate(bank10, 
-#                default = ifelse(default == "yes", 1, 0),
-#                housing = ifelse(housing == "yes", 1, 0),
-#                loan = ifelse(loan == "yes", 1, 0),
-#                 y = ifelse(y == "yes", 1, 0))
+bank <- bank %>% replace_with_na_all(condition = ~.x == "unknown")
+bank <-bank[complete.cases(bank10), ] 
 
 
 
-ggplot(mutate(bank10, job = fct_infreq(job))) + geom_bar(aes(x = job))
-bank10 <- bank10 %>% 
+bank <- bank %>% 
   mutate(Outcome = factor(y, levels = c("no","yes"),
-                        labels = c("Subscribed", "Not Subscribed")))
+                        labels = c("Not Subscribed", "Subscribed")))
 
 
-ggplot(bank10) + 
+bank <- transform( bank,
+                       job = ordered(job, levels = names( sort(-table(job)))))
+
+
+
+ggplot(bank) + 
   geom_bar(aes(x = job)) +
   facet_grid(. ~ Outcome) +
-  theme_few() 
+  theme_few()
 
 
 
-ggplot(mutate(bank10, education = fct_infreq(education))) + geom_bar(aes(x = education))
-ggplot(bank10) + 
+
+
+bank <- transform( bank,
+                   education = ordered(education, levels = names( sort(-table(education)))))
+
+ggplot(bank) + 
   geom_bar(aes(x = education)) +
   facet_grid(. ~ Outcome) +
   theme_few() 
 
+bank <- transform( bank,
+                   marital = ordered(marital, levels = names( sort(-table(marital)))))
 
 
-ggplot(mutate(bank10, marital = fct_infreq(marital))) + geom_bar(aes(x = marital))
-ggplot(bank10) + 
+ggplot(bank) + 
   geom_bar(aes(x = marital)) +
   facet_grid(. ~ Outcome) +
   theme_few() 
 
 
 
-ggplot(mutate(bank10, housing = fct_infreq(housing))) + geom_bar(aes(x = housing))
-ggplot(bank10) + 
-  geom_bar(aes(x = housing)) +
-  facet_grid(. ~ Outcome) +
-  theme_few() 
 
-ggplot(mutate(bank10, loan = fct_infreq(loan))) + geom_bar(aes(x = loan))
-ggplot(bank10) + 
+ggplot(mutate(bank, loan = fct_infreq(loan))) + geom_bar(aes(x = loan))
+ggplot(bank) + 
   geom_bar(aes(x = loan)) +
   facet_grid(. ~ Outcome) +
   theme_few() 
 
 
-ggplot(mutate(bank10, month = fct_infreq(month))) + geom_bar(aes(x = month))
-ggplot(bank10) + 
+bank <- transform( bank,
+                   month = ordered(month, levels = names( sort(-table(month)))))
+
+ggplot(bank) + 
   geom_bar(aes(x = month)) +
   facet_grid(. ~ Outcome) +
   theme_few() 
 
 
-ggplot(data = bank10) +
+ggplot(data = bank) +
   geom_bar(mapping = aes(x = education, fill = poutcome),position = "fill") +
   scale_fill_brewer( palette = "orange")
 
 
 
-# Cut age to different range
-bank10 <- bank10 %>% 
+# Cut age to different 
+bank <- bank %>% 
   mutate(age_distri = cut(age, c(20,40, 60, 80, 100)))
-summary(bank10)
+summary(bank)
 
 
 #Continue to dig in 
-ggplot(data = bank10) +
+ggplot(data = bank) +
   geom_bar(mapping = aes(x = age_distri, y = job, 
                          fill = Outcome), stat='identity', position ='dodge') +
   theme_few() +
@@ -109,4 +106,9 @@ ggplot(data = bank10) +
        y = "Job",
        x = "Age distribution",
        fill = "Outcome")  
+
+ggplot(data = bank, mapping = aes(x = loan, y = cons.conf.idx, color = Outcome)) +
+  geom_boxplot() +
+  facet_wrap(facets = vars(job)) 
+
 
